3897882c7cb587a32f72ed4ab5e32e531abad35b
vladimir.shmidt <vladimir.shmidt@totalobjects.spb.ru>
2015-08-27 11:12:19 +0300

    newproject

---
+++ Application.Tests/ProgramTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Moq;
+using NUnit.Framework;
+
+namespace Application.Tests
+{
+    [TestFixture]
+    public class ProgramTests
+    {
+        private readonly Mock<ICalculator> _calculator = new Mock<ICalculator>();
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            Program.SetContainer(new TestContainer(_calculator.Object));
+        }
+
+        [Test]
+        public void Should_Call_Calc()
+        {
+            Program.Main(new string[0]);
+            _calculator.Verify(c => c.Sum(It.IsAny<int>(), It.IsAny<int>()));
+        }
+    }
+
+    public class TestContainer : IFactoryContainer
+    {
+        private readonly ICalculator _calculator;
+
+        public TestContainer(ICalculator calculator)
+        {
+            _calculator = calculator;
+        }
+
+        public T Resolve<T>() where T : class
+        {
+            if (typeof(T) == typeof(ICalculator)) return _calculator as T;
+            return null;
+        }
+    }
+
+    
+}
---
+++ TestApplication/Application/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Application
+{
+    public class Program
+    {
+        static Program()
+        {
+            SetContainer(null);
+        }
+
+        public static void Main(string[] args)
+        {
+            ICalculator calc = ObjectFactory.Current.Resolve<ICalculator>();
+            calc.Sum(10, 100);
+        }
+
+        public static void SetContainer(IFactoryContainer container)
+        {
+            ObjectFactory.SerCurrent(container ?? new ConsoleContainer());
+        }
+    }
+
+    public class ConsoleContainer : IFactoryContainer
+    {
+        public T Resolve<T>() where T : class
+        {
+            if (typeof(T) == typeof(ICalculator)) return new Calculator() as T;
+            throw new Exception();
+        }
+    }
+
+    public interface IFactoryContainer
+    {
+        T Resolve<T>() where T : class;
+    }
+
+    public class ObjectFactory
+    {
+        private static IFactoryContainer _current;
+
+        public static IFactoryContainer Current { get { return _current; } }
+
+        public static void SerCurrent(IFactoryContainer factory)
+        {
+            _current = factory;
+        }
+    }
+
+    public class Calculator : ICalculator
+    {
+        public void Sum(int a, int b)
+        {
+            var sum = a + b;
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+
+    public interface ICalculator
+    {
+        void Sum(int a, int b);
+    }
+}
